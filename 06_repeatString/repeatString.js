74ef8996df5b6b225d8f0c2acb8a90781dda8a93
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-06-03 16:35:13 +0800

    completed exercises 6-7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    let result = "";
+    if (b<0) {return 'ERROR'}
+    else
+    for (let i = 0; i<b; i++){
+        result += a;
+    }
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(ReverseMe) {
+    let start = ReverseMe.length;
+    let string = "";
+    for (let i = start; i > 0; i--){
+    string += ReverseMe[i - 1];
+    }
+    return string;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
