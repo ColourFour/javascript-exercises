06a2900ca9600d3c9c22e34d91920f9c6604d516
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-06-04 10:45:35 +0800

    completed exercises 8-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function([a],b) {
+    let start = a.length;
+    for (i = 1; i< (start + 1); i++){
+        if (a.i == b){
+            a.splice(i, 1);
+    }
+    return [a];
+}
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
